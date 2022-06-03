48b3158b9c9712023dab8e3d4d0fcb0d62ebe196
Seung Lee <shl7@uw.edu>
2022-06-03 11:22:38 -0700

    Add third question

--- js/scripts.js
+++ js/scripts.js
@@ -3,6 +3,7 @@ $(document).ready(function () {
     event.preventDefault();
     const name = $('#name').val();
     const q1 = $('input:radio[name=q1]:checked').val();
+    const q3 = $('input:radio[name=q3]:checked').val();
     console.log(name);
     console.log(q1);
 
@@ -12,6 +13,9 @@ $(document).ready(function () {
     if (q1 === 'option3') {
       resultTitle += 'MACHINE LANGUAGE';
       resultReason = 'You like 0s and 1s. Just like a machine.';
+    } else if (q3 === 'option2') {
+      resultTitle += 'COBOL';
+      resultReason = 'COBOL and Cobalt. Basically same right?';
     }
 
     $('#result-title').text(resultTitle);
