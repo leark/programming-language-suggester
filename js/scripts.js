3e013810295bc01f9022e0ab5a3da8992ff6291f
Seung Lee <shl7@uw.edu>
2022-06-03 11:01:33 -0700

    Add first question

--- js/scripts.js
+++ js/scripts.js
@@ -5,5 +5,16 @@ $(document).ready(function () {
     const q1 = $('input:radio[name=q1]:checked').val();
     console.log(name);
     console.log(q1);
+
+    let resultTitle = "CONGRATULATIONS, LANGUAGE YOU'RE MOST ATTUNED TO IS ";
+    let resultReason;
+
+    if (q1 === 'option3') {
+      resultTitle += 'MACHINE LANGUAGE';
+      resultReason = 'You like 0s and 1s. Just like a machine.';
+    }
+
+    $('#result-title').text(resultTitle);
+    $('#result-reason').text(resultReason);
   });
 });
